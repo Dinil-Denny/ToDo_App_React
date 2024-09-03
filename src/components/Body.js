2f825cc73b32f73ba2d6bf6bb800cc643878332a
Dinil_Denny <dinildenny99@gmail.com>
2024-09-04 02:10:59 +0530

    mark as done and delete added

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,8 +3,24 @@ import { useState } from "react";
 
 const Body = ()=>{
 
-const [toDoList,setToDoList] = useState([]);
-const [task,setTask] = useState('');
+    const [toDoList,setToDoList] = useState([]);
+    const [task,setTask] = useState('');
+
+    //this function will toggle the 'done' status of each tasks
+    const toggleDone = (idx,updatedTaskObj)=>{
+        //if the index of object is equal to 
+        let updatedTask = toDoList.map((taskObj,index) => (index===idx) ? updatedTaskObj : taskObj);
+        setToDoList(updatedTask);
+    }
+
+    //here we filter and find all objects other than the object with passed in index. 
+    //using array.filter method will return an array of objects other than specified index's object.
+    //then update the toDoList state variable with this array of objects.
+    // Now automatically the object with specified index will be removed
+    const deleteTask = (idx)=>{
+        const filteredList = toDoList.filter((_,index)=>(index !== idx));
+        setToDoList(filteredList);
+    }
 
     return(
         <div id="bodyDiv">
@@ -13,18 +29,17 @@ const [task,setTask] = useState('');
                     setTask(e.target.value);
                 }}></input>
                 <button id="addTaskBtn" onClick={()=>{
-                    setToDoList([...toDoList,{id:Date.now(),taskName:task,done:false}]);
-                    
-                }}>Add Task</button>
+                    setToDoList([...toDoList,{taskName:task,done:false}]);
+                    setTask('');
+                }}>Add Task</button>    
             </div>
             <div id="toDoList">
                 {console.log(toDoList)}
                 {
-                    toDoList.map((toDo)=>(
-                        <ToDoComponent key={toDo.id} tasks = {toDo}/>
+                    toDoList.map((toDo,index)=>(
+                        <ToDoComponent key={index} task={toDo} index={index} toggleDone={toggleDone} deleteTask={deleteTask}/>
                     ))
                 } 
-                {/* <ToDoComponent tasks = {toDoList}/> */}
             </div>
         </div>
     )
--- src/components/ToDoComponent.js
+++ src/components/ToDoComponent.js
@@ -1,13 +1,28 @@
 import React from "react";
+import {FaCheck, FaTrash} from "react-icons/fa";
 
-const ToDoComponent = ({tasks}) => {
-    console.log("task:",tasks);
+const ToDoComponent = ({task,index,toggleDone,deleteTask}) => {
+    //this handleToggle function calls the togleDone function with parameters, 
+    //index of the task object displaying and an updated task object with 'done' updated to a value just opposite of its initial value 
+    const handleToggle = ()=>{
+        toggleDone(index,{...task,done:!task.done});
+    }
+
+    const handleDelete = ()=>{
+        deleteTask(index);
+    }
+
+    console.log("task:",task);
+    console.log("index:",index);
     return(
         <div id="todos">
-            {/* {tasks.map((task)=>
-                <h1 className="taskText">{task.taskName}</h1>
-            )} */}
-            <p className="taskText">{tasks.taskName}</p>
+            {(task.done)?<s className="taskText">{task.taskName}</s>:<p className="taskText">{task.taskName}</p>}
+            <div>
+                {/* while clicking tick icon handleToggle function is called*/}
+                <FaCheck className="icon" onClick={handleToggle}/>
+                {/* while clicking trash icon handleDelete function is called */}
+                <FaTrash className="icon" onClick={handleDelete}/>  
+            </div>
         </div>
     )
 }
